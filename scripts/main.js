76a95920659216709b16bbff3012a00b30167050
Nick Anderson <njcanderson@gmail.com>
2024-07-03 13:15:24 -0400

    Fill in details about the game

--- scripts/main.js
+++ scripts/main.js
@@ -7,24 +7,19 @@ gameButton.addEventListener("click", showGame);
 const statsButton = document.getElementById("stats-button");
 statsButton.addEventListener("click", showStats);
 
-showGame();
-
 function showAbout() {
-    console.log("showing about");
     $("#about").show();
     $("#game").hide();
     $("#stats").hide();
 }
 
 function showGame() {
-    console.log("showing game");
     $("#about").hide();
     $("#game").show();
     $("#stats").hide();
 }
 
 function showStats() {
-    console.log("showing stats");
     $("#about").hide();
     $("#game").hide();
     $("#stats").show();
